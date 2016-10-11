aa27f4b1908bbc26ab12ddcbbfb1aed05eb7ed72
GuruVII <guruvii@gmail.com>
2016-10-11 14:05:10 +0200

    changed the cookie design with localStorage, fixed issue with selecting words

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,42 +1,18 @@
-var masterVerbArray = [[0, "sedeti", "sit", "sat", "sat"], [1, "biti", "be", "was", "been"], [2, "voziti", "drive", "drove", "driven"]];
-var cookiesArray = [0.01, 0.01, 0.01]; //placeholder, the proper array will besotred in a cookie
+var masterVerbArray = [[0, "sedeti", "sit", "sat", "sat", 0.2], [1, "biti", "be", "was", "been", 0.2], [2, "voziti", "drive", "drove", "driven", 0.01], [3, "jesti","eat","ate", "eaten", 0.01]];
 var testVerbs =[] //the array will contain verbsthat wil be used in testing
 
-function MergeArrays(){ //this function mergers two array that is stored in a cookie and the array inside the javascriptfile
-	var tempArray = masterVerbArray; //master array must always be intact
-	tempArray.forEach(function(currentValue, index){ 
-		currentValue.push(cookiesArray[index]); //pushing the number from the cookies array into the subarray of the tempArray, from which we shall create a merged array
-			});
-	return tempArray
-};
-
-
-function WordSelect(){ //This funtion selects the words we are going to use
-	var tempArray = MergeArrays();
-	var selectedWords =[];
- 	function arraySearch(){
- 		var dieRoll = Math.random(); //comare a single dieroll to all the entries inside the tempArray, since it decrease the amounts of rolls the computer must do
-		for (var i = 0; i < tempArray.length; i++){//continusly cycle though the array while the words haven't been selected yet
-				//random roll, the die is case once for each gothough the temp arrray
-			console.log(dieRoll);
-			if (dieRoll <= tempArray[i][5]){ //if the mathrandom is smaller or equal to chance from the cookies array
-				selectedWords.push(tempArray[i]);
-				console.log(tempArray[i]);
-				console.log(tempArray[i][0]);
-				tempArray.splice(tempArray[i], 1);
-				if(selectedWords.length >= 2){
-					return;
-				}
-				else{
-					arraySearch() //if the selectedWords array isn't full, we need to call the arraySsearch function again
-				};
-			}
-
-		};
+function createVerbStorage() { //function checks if there is already a verbArray in local storage and if there isn't creates one
+	localStorage.clear(); 
+	if (localStorage.getItem("verbArray") === null) {
+		localStorage["verbArray"] = JSON.stringify(masterVerbArray);
 	};
-	arraySearch();
+};
+function WordSelect(){ //This funtion selects the words we are going to use
+	var tempArray = JSON.parse(localStorage.getItem('verbArray'));
+	var selectedWords =[];
+	arraySearch(tempArray, selectedWords);
 	if(selectedWords.length < 2){
-		arraySearch() //if, no die rolls fit the changes on this passthough, we need to call the function again
+		arraySearch(tempArray, selectedWords) //if, no die rolls fit the changes on this passthough, we need to call the function again
 	};
 	console.log(selectedWords);
 	console.log(tempArray);
@@ -50,8 +26,27 @@ function WordSelect(){ //This funtion selects the words we are going to use
 	console.log(tempArray);
 };
 
+
+var arraySearch =function (tempArray,selectedWords){
+ 		var dieRoll = Math.random(); //compare a single dieroll to all the entries inside the tempArray, since it decrease the amounts of rolls the computer must do
+		for (var i = 0; 2 > selectedWords.length; i++){//continusly cycle though the array while the words haven't been selected yet
+				//random roll, the die is case once for each gothough the temp arrray
+			console.log(dieRoll);
+			if (dieRoll <= tempArray[i][5]){ //if the mathrandom is smaller or equal to chance from the verbArray
+				selectedWords.push(tempArray[i]);
+				console.log(tempArray[i]);
+				console.log(tempArray[i][0]);
+				tempArray.splice(tempArray[i], 1);
+			};
+			console.log("selected words length:" + selectedWords.length);
+			if(i == (tempArray.length-1)){
+					i = 0;
+					var dieRoll = Math.random();
+			};
+		};
+	};
 /*function WordCompare (){
 	if ($("present-simple").text == )
 }*/
-
+createVerbStorage();
 WordSelect();
